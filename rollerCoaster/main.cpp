818adc1812ba5b96ee4b792fd214834c72f2c1e4
安伯川 <xiaoyang@xiaoyus-iMac.local>
2017-05-31 22:57:35 -0400

    Initial Commit

---
+++ rollerCoaster/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rollerCoaster
+//
+//  Created by xiaoyu on 2017-05-31.
+//  Copyright © 2017 xiaoyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
